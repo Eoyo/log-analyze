3af81c450927e55db62e564b0d815c7507835b05
刘淼 <liumiao@cvte.com>
2020-03-18 11:22:06 +0800

    [add]补充了强大的过滤语法

--- code/clear-rubbish.ts
+++
@@ -1,4 +0,0 @@
-import fs from "fs-extra";
-import cur from "../src/libs/cur";
-
-fs.emptyDir(cur("./rubbish"));
--- src/log.ts
+++ src/log.ts
@@ -1,6 +1,7 @@
 import fs from "fs-extra";
 import readline from "readline";
 import path from "path";
+import chalk from "chalk";
 // @ts-ignore
 import Nzh from "nzh";
 
@@ -9,11 +10,30 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-async function readLogFileData(file: string) {
+type LogRecord = {
+  time: string;
+  level: string;
+  message: string;
+  isMark: boolean;
+};
+
+async function readLogFileData(file: string): Promise<LogRecord[]> {
   const data = await fs.readFile(file);
-  return data
+  const logs = data
     .toString()
     .split(path.basename(file).split(".")[0] || Date.now() + "");
+
+  return logs
+    .filter((one) => !!one)
+    .map((one) => one.trim())
+    .map((one) => {
+      return {
+        time: one.slice(0, 12),
+        level: one.slice(13, 14),
+        message: one.slice(15),
+        isMark: false,
+      };
+    });
 }
 
 async function getFilesInDir(dir: string) {
@@ -37,53 +57,165 @@ async function readAllLogsFile() {
 }
 
 function createIgnoreStrMatch(str: string) {
-  return new RegExp(str, "i");
+  return new RegExp(str.replace("_", "[_ ]"), "i");
 }
 
-function allRegMatchStr(reg: RegExp[]) {
-  return (str: string) => {
-    const testResult = !reg.some((oneReg) => {
-      return !oneReg.test(str);
-    });
-    // console.log("testing", str, testResult);
-    return testResult;
-  };
+function allRegMatchStr(reg: RegExp[]): StrFilter {
+  return Object.assign(
+    (str: string) => {
+      const testResult = !reg.some((oneReg) => {
+        return !oneReg.test(str);
+      });
+      // console.log("testing", str, testResult);
+      return testResult;
+    },
+    {
+      hasReg: reg.length > 0,
+    }
+  );
 }
 
-function searchKeywordsInData(data: string[]) {
-  return function searchKeywords(props: string[]) {
-    console.log("searching... ", props);
-    const reg = props.map(createIgnoreStrMatch);
-    console.log("use regexp:", reg);
-    const result = data
-      .filter(allRegMatchStr(reg))
-      .map((one) => one.trim())
-      .filter((one) => !!one);
-    // console.log("search result", result);
-    return result;
-  };
-}
-
-function handleSearchingWith(
-  searchKeywords: (inputData: string[]) => string[]
-) {
+function inputLine(inputHandler: (inputStr: string) => void) {
   console.log("\n请输入关键字搜索, 回车确定:");
   rl.on("line", (input) => {
-    const inputData = input.split(" ").filter((one) => !!one);
-    const result = searchKeywords(inputData);
-    fs.writeFile(
-      "./output.log",
-      `结果: ${Nzh.cn.encodeS(result.length)} 条日志\n` + result.join("\n")
-    ).then(() => {
-      console.log(`结果: ${Nzh.cn.encodeS(result.length)} 条日志`);
-    });
+    inputHandler(input);
   });
+}
+
+function logRecordStr(log: LogRecord): string {
+  const message = `${log.time} ${log.level} ${log.message}`;
+  if (log.isMark) {
+    return `\n<<<\n${message}\n>>>\n`;
+  } else {
+    return message;
+  }
+}
+
+async function writeLogsRecord(data: LogRecord[]) {
+  const messages = data.map(logRecordStr);
+  await fs.writeFile(
+    "./output.log",
+    `结果: ${Nzh.cn.encodeS(messages.length)} 条日志\n` + messages.join("\n")
+  );
+  console.log(`结果: ${Nzh.cn.encodeS(messages.length)} 条日志`);
+}
+
+type SearchFilters<T = StrFilter> = {
+  time: T;
+  level: T;
+  content: T;
+  mark: T;
+};
+
+type StrFilter = { (str: string): boolean; hasReg: boolean };
+
+function logFilters(originStr: string, filters: SearchFilters<string[]>) {
+  console.log(chalk.blue("filters: "), chalk.green(originStr));
+  const keys = Object.keys(filters);
+  keys.forEach((oneKey) => {
+    console.log(chalk.blueBright(oneKey) + "\t", (filters as any)[oneKey]);
+  });
+}
+
+function stringArrayFilter(str?: string): string[] {
+  if (str) {
+    return str
+      .split(" ")
+      .map((one) => one.trim())
+      .filter((one) => !!one);
+  } else {
+    return [];
+  }
+}
+
+/**
+ * 通过 字符串 构建搜索的过滤器
+ */
+function searchFilters(searchStr: string): SearchFilters<StrFilter> {
+  const commandRegArray = searchStr.match(/`.*`/);
+  const filters: SearchFilters<string[]> = {
+    time: [],
+    level: [],
+    content: [],
+    mark: [],
+  };
+  let commandStr = "";
+  if (commandRegArray) {
+    const t1 = commandRegArray[0];
+    commandStr = t1;
+    const t3 = t1.slice(1, -1).split(" ");
+    t3.forEach((one) => {
+      if (one) {
+        if (one.match(/[0-9]/)) {
+          filters.time.push(one);
+        } else {
+          filters.level.push(one);
+        }
+      }
+    });
+  }
+  const t2 = commandStr ? searchStr.replace(commandStr, "") : searchStr;
+  const [contentStr, markStr] = t2.split(">>>").map((one) => one.trim());
+  const contentStrArray = stringArrayFilter(contentStr);
+  const markStrArray = stringArrayFilter(markStr);
+  if (contentStrArray) {
+    filters.content = contentStrArray;
+  }
+  if (markStrArray) {
+    filters.mark = markStrArray;
+  }
+
+  const { level, mark, time, content } = filters;
+  logFilters(searchStr, filters);
+
+  const [levelReg, markReg, timeReg, contentReg] = [level, mark, time, content]
+    .map((one) => {
+      return one.map(createIgnoreStrMatch);
+    })
+    .map(allRegMatchStr);
+
+  const filtersReg = {
+    level: levelReg,
+    time: timeReg,
+    mark: markReg,
+    content: contentReg,
+  };
+  return filtersReg;
+}
+
+function filterLogsRecord(
+  data: LogRecord[],
+  filters: SearchFilters
+): LogRecord[] {
+  let d = data;
+
+  if (filters.level.hasReg) {
+    d = d.filter((one) => filters.level(one.level));
+  }
+
+  if (filters.content.hasReg) {
+    d = d.filter((one) => filters.content(one.message));
+  }
+  if (filters.time.hasReg) {
+    d = d.filter((one) => filters.time(one.time));
+  }
+
+  if (filters.mark.hasReg) {
+    d.forEach((one) => {
+      one.isMark = filters.mark(one.message);
+    });
+  }
+  return d;
 }
 
 async function start() {
   const data = await readAllLogsFile();
+  console.log(data[1]);
   console.log("文件解析完成");
-  handleSearchingWith(searchKeywordsInData(data));
+  inputLine((inputStr) => {
+    const filters = searchFilters(inputStr);
+    writeLogsRecord(filterLogsRecord(data, filters));
+  });
 }
 
 start();
