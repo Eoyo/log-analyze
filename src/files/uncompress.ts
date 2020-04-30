b7fabcb87f782582b00f029ac044321b7e2a601d
刘淼 <liumiao@cvte.com>
2020-04-30 17:49:13 +0800

    [fix]  windows 中打包的日志可能为uploading开头

--- src/files/uncompress.ts
+++ src/files/uncompress.ts
@@ -3,8 +3,8 @@ import * as compressing from "compressing"
 import path from "path"
 import fs from "fs-extra"
 
-const tempUnCompressFileDir = "un-compressing-temp"
-const mindlinkerLogReg = /(main\.log|render\.log)\.[0-9]*\.gz/g
+const tempUnCompressFileDir = ".temp"
+const mindlinkerLogReg = /(main\.log|render\.log)\.[0-9]*\.gz/
 
 export async function isTar(filePath: string) {
   if (path.extname(filePath) === ".tar") {
@@ -20,32 +20,49 @@ export async function unCompressTarFile(filePath: string) {
   }
 
   const destDir = filePath
-  await fs.ensureDir(destDir)
 
   const tempDir = path.join(tempUnCompressFileDir, path.basename(filePath))
   await compressing.tar.uncompress(filePath, tempDir)
+  await fs.remove(filePath)
+  await fs.ensureDir(destDir)
 
-  const gzFileDir = path.join(tempDir, "uploading")
-  const files = await fs.readdir(gzFileDir)
   Promise.all(
-    files
-      .map((one) => path.join(gzFileDir, one))
-      .map((one) => {
-        return unCompressGZFileToLog(one, destDir)
-      })
+    (await getGZLogs(tempDir)).map((one) => {
+      return unCompressGZFileToLog(one, destDir, getAimLogName)
+    })
   )
-  // await fs.remove(filePath)
 }
 
-export async function unCompressGZFileToLog(filePath: string, destDir: string) {
+function getAimLogName(filePath: string) {
+  return path
+    .basename(filePath)
+    .replace("uploading\\", "")
+    .replace(mindlinkerLogReg, "$1")
+}
+
+async function getGZLogs(gzFileDir: string) {
+  if (await fs.pathExists(path.join(gzFileDir, "uploading"))) {
+    const p = path.join(gzFileDir, "uploading")
+    const files = await fs.readdir(gzFileDir)
+    return files
+      .filter((one) => mindlinkerLogReg.test(one))
+      .map((one) => path.join(p, one))
+  }
+  return (await fs.readdir(gzFileDir))
+    .filter((one) => mindlinkerLogReg.test(one))
+    .map((one) => path.join(gzFileDir, one))
+}
+
+export async function unCompressGZFileToLog(
+  filePath: string,
+  destDir: string,
+  getName: (fileName: string) => string
+) {
   if (!mindlinkerLogReg.test(filePath)) {
     console.log("ignore log:", filePath)
     return
   }
 
-  const aimFile = path.join(
-    destDir,
-    path.basename(filePath).replace(mindlinkerLogReg, "$1")
-  )
+  const aimFile = path.join(destDir, getName(filePath))
   await compressing.gzip.uncompress(filePath, aimFile)
 }
