62a008b4f0f63a9086f042161e82bbc1ca62bc17
刘淼 <liumiao@cvte.com>
2020-10-23 11:20:18 +0800

    [fix] 兼容只有一个 main.log 的情况.

--- src/logs-file/get-logs-files.ts
+++ src/logs-file/get-logs-files.ts
@@ -8,22 +8,32 @@ function getFileTimeName(name: string) {
 
 // 获取最新的日志文件.
 async function getFilesInDir(dir: string) {
+  let time = ""
   return (
     (await fs.readdir(dir))
       // 选择有时间的文件
       .filter((one) => !!getFileTimeName(one))
-      // 排序
+      // 排序, 最新的在前面.
       .sort((a, b) => {
         if (a > b) {
-          return 1
+          return -1
         }
         if (a === b) {
           return 0
         }
-        return -1
+        return 1
       })
       // 选择最新的两个
       .slice(-2)
+      // 选择时间一致的文件.
+      .filter((a) => {
+        const at = getFileTimeName(a)
+        if (time) {
+          return at === time
+        }
+        time = at
+        return true
+      })
       // 合并出最新路径
       .map((one) => path.join(dir, one))
   )
